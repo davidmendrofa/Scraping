c4c707d34bb8859741490566561db5fb755cfcb5
David Mendrofa <75975681+davidmendrofa@users.noreply.github.com>
2024-03-26 09:07:14 +0700

    Add files via upload

--- lamudi.py
+++ lamudi.py
@@ -13,64 +13,66 @@ from openpyxl.workbook import Workbook
 
 
 
-url = 'https://www.lamudi.co.id/yogyakarta/yogyakarta-1/house/buy/?page=1'
+url = 'https://www.lamudi.co.id/yogyakarta/yogyakarta-1/house/buy/?page='
 service = Service(executable_path = 'chromedriver.exe')
 driver = webdriver.Chrome(service = service)
 
-driver.get(url)
 # html_text = requests.get(url).text
 # print(html_text)
 
 data = []
+pages = 50
+count_pages = 0
+for page in range(1,51):
+    driver.get(url+str(page))
+    count_pages +=1
+    print('scraping on page:', count_pages)
+    try:
+        WebDriverWait(driver,10).until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR,'div.row.fullWidth.ClpBody'))
+        )
+        time.sleep(5)
 
-try:
-    WebDriverWait(driver,10).until(
-        EC.presence_of_all_elements_located((By.CSS_SELECTOR,'div.row.fullWidth.ClpBody'))
-    )
-    time.sleep(5)
 
+        soup = BeautifulSoup(driver.page_source,'lxml')
 
-    soup = BeautifulSoup(driver.page_source,'lxml')
+        for item in soup.findAll('div',class_='ListingCell-AllInfo ListingUnit'):
+            price = item.find('span',class_='PriceSection-FirstPrice').text.replace(' ','').replace('\n','').replace('Rp','')
+            try: 
+                bedroom_span = item.findAll('span', class_='KeyInformation-value_v2 KeyInformation-amenities-icon_v2')[0]
+                if bedroom_span:
+                    bedroom_icon = bedroom_span.find('span', class_='icon-bedrooms')
+                    if bedroom_icon:
+                        bedroom = bedroom_span.text.strip()
+                        bedroom = int(bedroom)
+            except: 
+                bedroom = ''
 
-    for item in soup.findAll('div',class_='ListingCell-AllInfo ListingUnit'):
-        price = item.find('span',class_='PriceSection-FirstPrice').text.replace(' ','').replace('\n','').replace('Rp','')
-        try: 
-            bedroom_span = item.findAll('span', class_='KeyInformation-value_v2 KeyInformation-amenities-icon_v2')[0]
-            if bedroom_span:
-                bedroom_icon = bedroom_span.find('span', class_='icon-bedrooms')
-                if bedroom_icon:
-                    bedroom = bedroom_span.text.strip()
-                    bedroom = int(bedroom)
-        except: 
-            bedroom = ''
+            try:
+                livingsize_span = item.findAll('span', class_='KeyInformation-value_v2 KeyInformation-amenities-icon_v2')[1]
+                if livingsize_span:
+                    livingsize_icon = livingsize_span.find('span', class_='icon-livingsize')
+                    if livingsize_icon:
+                        livingsize = livingsize_span.text.replace('m²','').strip()
+                        livingsize = int(livingsize)
+            except: 
+                livingsize = ''
 
-        try:
-            livingsize_span = item.findAll('span', class_='KeyInformation-value_v2 KeyInformation-amenities-icon_v2')[1]
-            if livingsize_span:
-                livingsize_icon = livingsize_span.find('span', class_='icon-livingsize')
-                if livingsize_icon:
-                    livingsize = livingsize_span.text.replace('m²','').strip()
-                    livingsize = int(livingsize)
-        except: 
-            livingsize = ''
-
-        try:
-            landsize_span = item.findAll('span', class_='KeyInformation-value_v2 KeyInformation-amenities-icon_v2')[2]
-            if landsize_span:
-                landsize_icon = landsize_span.find('span',class_='icon-land_size')
-                if landsize_icon:
-                    landsize = landsize_span.text.replace('m²','').strip()
-                    landsize = int(landsize)
-        except: 
-            landsize = ''
+            try:
+                landsize_span = item.findAll('span', class_='KeyInformation-value_v2 KeyInformation-amenities-icon_v2')[2]
+                if landsize_span:
+                    landsize_icon = landsize_span.find('span',class_='icon-land_size')
+                    if landsize_icon:
+                        landsize = landsize_span.text.replace('m²','').strip()
+                        landsize = int(landsize)
+            except: 
+                landsize = ''
+                
             
-        
-        data.append((price,bedroom,livingsize,landsize))
+            data.append((price,bedroom,livingsize,landsize))
     
-    # driver.find_element()
-
-except TimeoutException:
-    print("Timeout: Unable to find the expected elements within the specified time.")
+    except TimeoutException:
+        print("Timeout: Unable to find the expected elements within the specified time.")
 
 df = pd.DataFrame(data, columns=['harga_rp','kamar_tidur','luas_bangunan_m²','luas_tanah_m²'])
 df.to_excel('Harga Rumah Yogyakarta di Lamudi.xlsx', index=False)
